ab89ac4e80bdaa4159ca9f88a38cee365b51e0ac
seokhoonhyeon <seokhoon.hyeon@gmail.com>
2026-01-06 17:24:56 +0900

    chore : 프로젝트 세팅

---
+++ src/test/java/sparta/firstevent/FirsteventApplicationTests.java
@@ -0,0 +1,13 @@
+package sparta.firstevent;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FirsteventApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
